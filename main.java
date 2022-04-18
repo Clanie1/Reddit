c509d88178b4b38b6abc180d6eee2c6baf3e53f5
Clanie1 <danybarociov@gmail.com>
2022-04-18 09:37:48 -0700

    hoal

--- main.java
+++ main.java
@@ -1,8 +1,10 @@
+import java.io.FileNotFoundException;
 
 public class main {
 
-  public static void main(String[] args) {
-
+  public static void main(String[] args) throws FileNotFoundException {
+    postAdmin cosa = new postAdmin();
+    postAdmin.loadPosts();
   }
 
 }
--- postAdmin.java
+++ postAdmin.java
@@ -1,7 +1,28 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Random;
+
 public class postAdmin {
   Post[] post;
 
-  public static void loadPosts() {
+  String file = "./Documentos/posts.daniel";
+
+  public static void loadPosts() throws FileNotFoundException {
+
+    File file = new File("./Documentos/posts.daniel");
+    Scanner scanner = new Scanner(file);
+    Random rad = new Random();
+    int num = rad.nextInt(4);
+    System.out.println(num);
+    String line = "";
+    for (int i = 0; i < num; i++) {
+      line = scanner.nextLine();
+    }
+    String[] postInfo = line.split(",");
+    for (String info : postInfo) {
+      System.out.println(info);
+    }
 
   }
 
