457228dcb111e2a6c1df4d3ccec5630cb0735489
el-shago <sperezmaris@gmail.com>
2022-04-19 11:26:10 -0700

    coochieman
    Ya te registrastes broder?

--- IncioSesion.java
+++ IncioSesion.java
@@ -3,6 +3,22 @@ import java.util.Scanner;
 public class IncioSesion {
   // constructor
   public IncioSesion() {
+  String res;
+  Scanner read = new Scanner(System.in);
+  System.out.println("Ya estas registrado?");
+  res = read.nextLine();
+  if(res.equals("Si")|| res.equals("si")|| res.equals ("S")|| res.equals ("s")){
+  //login()
+  }
+  else if(res.equals("No")||res.equals ("no")|| res.equals("N")||res.equals("n")){
+  //registro()
+  }
+  else{
+System.out.println("Introduce una respuesta valida por favor");
+  }
+  }
+  
+
 
   }
-}
+
